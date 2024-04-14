9b38a5c5724e9349334245e3fe7ccb2436783f9b
nhixuannt <167008123+nhixuannt@users.noreply.github.com>
2024-04-15 00:17:40 +0700

    Add files via upload

---
+++ [Canhan]Mang1chieu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace _Canhan_Mang1chieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Khai báo biến
+            int t;
+            string thang;
+            // Output tiếng việt có dấu
+            Console.OutputEncoding = Encoding.Unicode;
+            // Vòng lặp bắt lỗi nhập sai dữ liệu
+            do
+            {
+                Console.Write("Nhập vào tháng bất kì trong năm dưới dạng số từ 1-12:");
+                thang = Console.ReadLine();
+                int.TryParse(thang, out t);
+                if (t <= 0 || t > 12) Console.WriteLine("\nDữ liệu nhập vào phải là số từ 1-12, vui lòng nhập lại!!!\n");
+            }
+            while (t <= 0 || t > 12);
+            // Khai báo mảng gán các giá trị số ngày
+            int[] songay = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            // In ra màn hình đáp án bài toán
+            Console.WriteLine("Số ngày của tháng {0} là {1}", t, songay[t - 1]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ [Canhan]Matrix/Program.cs
@@ -0,0 +1,159 @@
+﻿using System.Text;
+
+namespace _Canhan_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Khai báo biến dòng và cột của 2 ma trận A và B
+            int m, n1, n2, p;
+            // Output hỗ trợ tiếng việt
+            Console.OutputEncoding = Encoding.Unicode;
+            // Vòng lặp bắt lỗi nhập sai dữ liệu số dòng, số cột của ma trận A
+            do
+            {
+                Console.Write("Nhập vào số dòng của ma trận A:");
+                string dA = Console.ReadLine();
+                Console.Write("Nhập vào số cột của ma trận A:");
+                string cA = Console.ReadLine();
+                int.TryParse(dA, out m);
+                int.TryParse(cA, out n1);
+                if (m <= 0 || n1 <= 0 || (m <= 0 && n1 <= 0))
+                {
+                    Console.WriteLine("Dữ liệu nhập vào sai, vui lòng nhập lại!!!");
+                }
+            }
+            while (m <= 0 || n1 <= 0 || (m <= 0 && n1 <= 0));
+        // Vòng lặp bắt lỗi nhập sai dữ liệu các phần tử của ma trận A
+        checkA:
+            int[,] A = new int[m, n1];
+            string[,] A1 = new string[m, n1];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n1; j++)
+                {
+                    Console.Write("Nhập vào phần tử A[{0},{1}]: ", i, j);
+                    A1[i, j] = Console.ReadLine();
+                }
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n1; j++)
+            {
+                    foreach (char pt in A1[i, j])
+                    {
+                        if (!char.IsDigit(pt))
+                        {
+                            Console.WriteLine("Dữ liệu nhập vào sai, vui lòng nhập lại!!!");
+                            goto checkA;
+                        }
+                        else
+                        {
+                            A[i, j] = int.Parse(A1[i, j]);
+                        }
+                    }
+                }
+            }
+            // In ma trận A
+            Console.WriteLine("\nMa trận A:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n1; j++)
+                {
+                    Console.Write("{0} ", A1[i, j]);
+                }
+                Console.WriteLine("");
+            }
+            // Vòng lặp bắt lỗi nhập sai dữ liệu số dòng, số cột của ma trận B
+            do
+            {
+                Console.WriteLine("");
+                Console.Write("Nhập vào số dòng của ma trận B:");
+                string dB = Console.ReadLine();
+                Console.Write("Nhập vào số cột của ma trận B:");
+                string cB = Console.ReadLine();
+                int.TryParse(dB, out n2);
+                int.TryParse(cB, out p);
+                if (n2 <= 0 || p <= 0 || (n2 <= 0 && p <= 0))
+                {
+                    Console.WriteLine("Dữ liệu nhập vào sai, vui lòng nhập lại!!!");
+                }
+            else
+                {
+                    if (n2 != n1)
+                    {
+                        Console.WriteLine("\nKhông thể nhân 2 ma trận trên. Vui lòng nhập lại!");
+                       
+                        Console.Write("Số dòng của ma trận B phải bằng số cột của ma trận A");
+                    }
+                }
+            }
+            while (n2 <= 0 || p <= 0 || (n2 <= 0 && p <= 0) || n2 != n1);
+        // Vòng lặp bắt lỗi nhập sai dữ liệu các phần tử của ma trận B
+        checkB:
+            int[,] B = new int[n2, p];
+            string[,] B1 = new string[n2, p];
+            for (int i = 0; i < n2; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    Console.Write("Nhập vào phần tử B[{0},{1}]: ", i, j);
+                    B1[i, j] = Console.ReadLine();
+                }
+            }
+            for (int i = 0; i < n2; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    foreach (char pt in B1[i, j])
+                    {
+                        if (!char.IsDigit(pt))
+                        {
+                            Console.WriteLine("Dữ liệu nhập vào sai, vui lòng nhập lại!!!");
+                            goto checkB;
+                        }
+                        else
+                        {
+                            B[i, j] = int.Parse(B1[i, j]);
+                        }
+                    }
+                }
+            }
+            // In ma trận B
+            Console.WriteLine("\nMa trận B:");
+            for (int i = 0; i < n2; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    Console.Write("{0} ", B[i, j]);
+                }
+                Console.WriteLine("");
+            }
+            // Tính toán các phần tử của ma trận tích
+            double[,] AB = new double[m, p];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    AB[i, j] = 0;
+                    for (int k = 0; k < n2; k++)
+                    {
+                        AB[i, j] += A[i, k] * B[k, j];
+                    }
+                }
+            }
+            // In ma trận tích AB 
+            Console.WriteLine("\nTích của 2 ma trận trên là: ");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    Console.Write("{0} ", AB[i, j]);
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
